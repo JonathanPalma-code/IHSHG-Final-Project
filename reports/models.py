3ccfa5842ae8c0ccd4daf73bf3733e7e13234b8a
Jonathan <jonathanpalma89@gmail.com>
2021-11-08 18:11:06 +0000

    chore: add user model and migrate

--- reports/models.py
+++ reports/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+class User(AbstractUser):
+    birth_date = models.DateField()
+    is_active = models.BooleanField(default=False)
+    reports = models.IntegerField(default=0)
+
+    def serialize(self):
+        return {
+            'id': self.id,
+            'username': self.username,
+            'email': self.email,
+            'birth date': self.dateFormat(),
+            'is_active': self.is_active,
+            'reports': self.reports,
+        }
+
+    def dateFormat(self):
+        if self.birth_date:
+            return self.birth_date.strftime('%b %#d %Y')
+
+    def __str__(self):
+        return f'{self.email}'
