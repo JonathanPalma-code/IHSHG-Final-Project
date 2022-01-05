ae90b71a86ff49d022023a0647810fd9612d2188
Jonathan <jonathanpalma89@gmail.com>
2022-01-05 17:37:14 +0000

    chore: add Profile and edit data

--- IHSHG/settings.py
+++ IHSHG/settings.py
@@ -122,5 +122,8 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 # Email Setup
-
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+# Upload Media
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
--- account/views.py
+++ account/views.py
@@ -2,7 +2,33 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.decorators import login_required
-from .forms import LoginForm, UserRegistrationForm
+from .forms import LoginForm, UserRegistrationForm, UserEditForm, ProfileEditForm
+from .models import Profile
+
+@login_required
+def home(request):
+    return render(request, 'account/index.html', {'section': 'dashboard'})
+
+@login_required
+def edit(request):
+    if request.method == 'POST':
+        user_form = UserEditForm(instance=request.user, data=request.POST)
+        profile_form = ProfileEditForm(instance=request.user.profile,
+                                        data=request.POST,
+                                        files=request.FILES)
+                                        
+        if user_form.is_valid() and profile_form.is_valid():
+            user_form.save()
+            profile_form.save()
+    
+    else:
+        user_form = UserEditForm(instance=request.user)
+        profile_form = ProfileEditForm(instance=request.user.profile)
+    
+    return render(request, 'account/edit.html', {
+        'user_form': user_form,
+        'profile_form': profile_form
+    })
 
 def login_view(request):
     if request.method == "POST":
@@ -38,6 +64,7 @@ def register(request):
 
             # Save the user object
             new_user.save()
+            Profile.objects.create(user=new_user)
 
             return render(request, 'account/register_done.html', {
                 'new_user': new_user
@@ -47,8 +74,3 @@ def register(request):
     return render(request, 'account/register.html', {
         'user_form': user_form
     })
-
-@login_required
-def home(request):
-    return render(request, 'account/index.html', {'section': 'dashboard'})
-    
