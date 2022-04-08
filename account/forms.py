879ec1ffd2cfd15253ad78b0c8a843c0a9d36a05
Jonathan <jonathanpalma89@gmail.com>
2022-04-08 01:52:30 +0100

    style: registration form

--- account/forms.py
+++ account/forms.py
@@ -1,3 +1,5 @@
+from cProfile import label
+from operator import truediv
 from django import forms
 from django.contrib.auth.models import User
 from django.forms import fields
@@ -9,15 +11,26 @@ class LoginForm(forms.Form):
     password = forms.CharField(widget=forms.PasswordInput)
 
 class UserRegistrationForm(forms.ModelForm):
-    password = forms.CharField(label='Password',
-                               widget=forms.PasswordInput)
-    password2 = forms.CharField(label='Confirm Password',
-                                widget=forms.PasswordInput)
 
     class Meta:
         model = User
         fields = ('username', 'first_name', 'last_name', 'email')
 
+    # Loop to display all fields 
+    def __init__(self, *args, **kwargs):
+        super(UserRegistrationForm, self).__init__(*args, **kwargs)
+
+        for field_name, field in self.fields.items():
+            self.fields[field_name].widget.attrs['placeholder'] = field.label
+            self.fields[field_name].label = ''
+
+    password = forms.CharField(label='Password',
+        widget=forms.PasswordInput(attrs={'placeholder':'Password'}))
+
+    password2 = forms.CharField(label='Confirm Password',
+        widget=forms.PasswordInput(attrs={'placeholder': 'Confirm Password'}))
+
+    # Clean both fields if it is not a match
     def clean_password2(self):
         cd = self.cleaned_data
         if cd['password'] != cd['password2']:
@@ -28,6 +41,12 @@ class UserEditForm(forms.ModelForm):
     class Meta:
         model = User
         fields = ('first_name', 'last_name', 'email')
+    
+    # Disabled email field
+    def __init__(self, *args, **kwargs):
+        super(UserEditForm, self).__init__(*args, **kwargs)
+        
+        self.fields['email'].widget.attrs['disabled'] = True
 
 class ProfileEditForm(forms.ModelForm):
     class Meta:
--- account/views.py
+++ account/views.py
@@ -16,9 +16,28 @@ from .models import Profile
 from django.contrib import messages
 
 def home(request):
-    user_form = UserRegistrationForm()
+    registration_form = UserRegistrationForm() 
+    if request.method == 'POST':
+        registration_form = UserRegistrationForm(request.POST)
+        if registration_form.is_valid():
+
+            # Create a new user object but avoid saving it yet
+            new_user = registration_form.save(commit=False)
+
+            # Set the chosen password
+            new_user.set_password(registration_form.cleaned_data['password'])
+
+            # Save the user object
+            new_user.save()
+            Profile.objects.create(user=new_user)
+
+            return render(request, 'account/register_done.html', {
+                'new_user': new_user
+            })
+    else:
+        registration_form = UserRegistrationForm()
     return render(request, 'account/index.html', {
-        'user_form': user_form,
+        'registration_form': registration_form,
         'section': 'dashboard'
         })
 
